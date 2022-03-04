d268529e28f4a48d4408c0e0b3276e038024c33a
Sangmin-Jeong <Sangmin.Jeong@georgebrown.ca>
2022-03-04 13:36:20 -0500

    [ADD] PlatformPlayer.h

--- LE4_JeongSangmin/GAME1017_EmptyEngine/PlatformPlayer.cpp
+++ LE4_JeongSangmin/GAME1017_EmptyEngine/PlatformPlayer.cpp
@@ -1,1 +1,57 @@
 #include "PlatformPlayer.h"
+
+PlatformPlayer::PlatformPlayer(SDL_Rect s, SDL_FRect d)
+{
+}
+
+void PlatformPlayer::Update()
+{
+}
+
+void PlatformPlayer::Render()
+{
+}
+
+void PlatformPlayer::Stop()
+{
+}
+
+void PlatformPlayer::StopX()
+{
+}
+
+void PlatformPlayer::StopY()
+{
+}
+
+void PlatformPlayer::SetAccelX(double a)
+{
+}
+
+void PlatformPlayer::SetAccelY(double a)
+{
+}
+
+void PlatformPlayer::SetX(float x)
+{
+}
+
+void PlatformPlayer::SetY(float y)
+{
+}
+
+void PlatformPlayer::SetGrounded(bool g)
+{
+}
+
+void PlatformPlayer::IsGrounded()
+{
+}
+
+double PlatformPlayer::GetVelX()
+{
+}
+
+double PlatformPlayer::GetVelY()
+{
+}
