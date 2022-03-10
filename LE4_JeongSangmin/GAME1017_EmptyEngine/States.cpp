55f40fc726a2bd441d2d0b92ccba9ef88a8eebf9
Sangmin-Jeong <Sangmin.Jeong@georgebrown.ca>
2022-03-10 16:37:09 -0500

    [DONE] Lab 4 part 3

--- LE4_JeongSangmin/GAME1017_EmptyEngine/States.cpp
+++ LE4_JeongSangmin/GAME1017_EmptyEngine/States.cpp
@@ -110,6 +110,44 @@ void GameState::Update()
 		i.second->Update();
 		if (STMA::StateChanging()) return;
 	}
+	// Check Collision.
+	PlatformPlayer* pObj = static_cast<PlatformPlayer*>(GetGo("player"));
+	SDL_FRect* pBound = pObj->GetDst();
+	TiledLevel* pLevel = static_cast<TiledLevel*>(GetGo("level"));
+	for (unsigned int i = 0; i < pLevel->GetObstacles().size(); i++)
+	{
+
+		SDL_FRect* pTile = pLevel->GetObstacles()[i]->GetDst();
+		if (COMA::AABBCheck(*pBound, *pTile))
+		{
+			// If colliding with top side of tile
+			if ((pBound->y + pBound->h) - (float)pObj->GetVelY() <= pTile->y)
+			{
+				pObj->StopY();
+				pObj->SetY(pTile->y - pBound->h);
+				pObj->SetGrounded(true);
+			}
+			// If colliding with bottom side of tile
+			else if (pBound->y - (float)pObj->GetVelY() >= pTile->y + pTile->h)
+			{
+				pObj->StopY();
+				pObj->SetY(pTile->y + pTile->h);
+			}
+			// If colliding with left side of tile
+			else if ((pBound->x + pBound->w) - (float)pObj->GetVelX() <= pTile->x)
+			{
+				pObj->StopX();
+				pObj->SetX(pTile->x - pBound->w);
+			}
+			// If colliding with right side of tile
+			else if (pBound->x - (float)pObj->GetVelX() >= pTile->x + pTile->w)
+			{
+				pObj->StopX();
+				pObj->SetX(pTile->x + pTile->w);
+			}
+		}
+
+	}
 }
 
 void GameState::Render()
